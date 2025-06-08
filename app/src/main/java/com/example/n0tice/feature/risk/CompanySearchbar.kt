47b4e04c82a892248ebba64e0da89c7dd2c2a11e
romi <tofha1108@naver.com>
2025-06-08 23:07:11 +0900

    feat: 키보드 검색 버튼 클릭 시 검색 실행 및 키보드 숨김 처리 추가

--- app/src/main/java/com/example/n0tice/feature/risk/CompanySearchbar.kt
+++ app/src/main/java/com/example/n0tice/feature/risk/CompanySearchbar.kt
@@ -7,18 +7,25 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.text.KeyboardActions
+import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material3.Icon
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextField
 import androidx.compose.material3.TextFieldDefaults
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.MutableState
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalFocusManager
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.input.ImeAction
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.example.n0tice.R
@@ -27,7 +34,13 @@ import com.example.n0tice.core.ui.theme.WorkplaceSearchText
 import com.example.n0tice.core.ui.theme.preFontFamily
 
 @Composable
-fun CompanySearchbar(keyword: MutableState<String>) {
+fun CompanySearchbar(
+    search: (String) -> Unit
+) {
+    var keyword by remember { mutableStateOf("") }
+
+    val focusManager = LocalFocusManager.current
+
     Column(
         modifier = Modifier
             .fillMaxWidth()
@@ -60,8 +73,8 @@ fun CompanySearchbar(keyword: MutableState<String>) {
         }
 
         TextField(
-            value = keyword.value,
-            onValueChange = { keyword.value = it },
+            value = keyword,
+            onValueChange = { keyword = it },
             leadingIcon = {
                 Icon(
                     modifier = Modifier
@@ -72,6 +85,15 @@ fun CompanySearchbar(keyword: MutableState<String>) {
                     tint = WorkplaceSearchText
                 )
             },
+            keyboardOptions = KeyboardOptions(imeAction = ImeAction.Search),
+            keyboardActions = KeyboardActions(
+                onSearch = {
+                    if (keyword.isBlank()) return@KeyboardActions
+
+                    search(keyword)
+                    focusManager.clearFocus()
+                }
+            ),
             placeholder = {
                 Text(
                     text = "사업장을 입력하세요",
