2a046d4edf37f4ac6e0696a217bf74c1a95fa3d7
romi <tofha1108@naver.com>
2025-08-03 18:18:15 +0900

    feat: N0ticeClient HTTPS 검증 우회 (임시)

--- app/src/main/java/com/example/n0tice/core/api/n0tice/N0ticeClient.kt
+++ app/src/main/java/com/example/n0tice/core/api/n0tice/N0ticeClient.kt
@@ -4,21 +4,24 @@ import okhttp3.OkHttpClient
 import okhttp3.logging.HttpLoggingInterceptor
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
+import java.security.SecureRandom
+import java.security.cert.X509Certificate
+import javax.net.ssl.*
 
 object N0ticeClient {
     private var instance: Retrofit? = null
-    private const val BASE_URL = "http://52.79.71.10:8080/"
+    private const val BASE_URL = "https://52.79.71.10/"
 
     fun getInstance(): Retrofit {
-        val logging = HttpLoggingInterceptor().apply {
-            level = HttpLoggingInterceptor.Level.BODY // URL + 파라미터 + 응답 모두 출력
-        }
-
-        val client = OkHttpClient.Builder()
-            .addInterceptor(logging)
-            .build()
-
         if (instance == null) {
+            val logging = HttpLoggingInterceptor().apply {
+                level = HttpLoggingInterceptor.Level.BODY
+            }
+
+            val client = getUnsafeOkHttpClient().newBuilder()
+                .addInterceptor(logging)
+                .build()
+
             instance = Retrofit.Builder()
                 .baseUrl(BASE_URL)
                 .client(client)
@@ -26,5 +29,28 @@ object N0ticeClient {
                 .build()
         }
         return instance!!
+    }
+
+    private fun getUnsafeOkHttpClient(): OkHttpClient {
+        return try {
+            val trustAllCerts = arrayOf<TrustManager>(object : X509TrustManager {
+                override fun checkClientTrusted(chain: Array<X509Certificate>, authType: String) {}
+                override fun checkServerTrusted(chain: Array<X509Certificate>, authType: String) {}
+                override fun getAcceptedIssuers(): Array<X509Certificate> = arrayOf()
+            })
+
+            val sslContext = SSLContext.getInstance("SSL").apply {
+                init(null, trustAllCerts, SecureRandom())
+            }
+
+            val sslSocketFactory = sslContext.socketFactory
+
+            OkHttpClient.Builder()
+                .sslSocketFactory(sslSocketFactory, trustAllCerts[0] as X509TrustManager)
+                .hostnameVerifier { _, _ -> true } // 도메인 이름 검증도 우회
+                .build()
+        } catch (e: Exception) {
+            throw RuntimeException(e)
+        }
     }
 }
